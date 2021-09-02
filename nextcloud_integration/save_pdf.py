68951d3dfed3f9040f18304548205f6bdafadb63
al-chaudhari <chaudharia041@gmail.com>
2021-09-02 18:29:28 +0530

    added print attach

--- nextcloud_integration/save_pdf.py
+++ nextcloud_integration/save_pdf.py
@@ -1,4 +1,7 @@
 import frappe
+import requests
+from frappe.utils.file_manager import save_file, save_file_on_filesystem
+
 
 def save_pdf(doc, method=None):
 	"""
@@ -7,14 +10,22 @@ def save_pdf(doc, method=None):
 	if not is_doctype_allowed(doc):
 		return
 
-	print_format = get_print_format(doc)
+	print_format = get_print_format(doc.doctype)
+	pdf = get_pdf_data(doc.doctype, doc.name, print_format)
+	save_and_attach(pdf, doc.doctype, doc.name)
 
 
-def upload_pdf():
+def get_pdf_data(doctype, name, print_format):
 	"""
-	Upload PDF to Nextcloud
+	Get PDF Data
 	"""
-	pass
+	html = frappe.get_print(doctype, name, print_format=print_format)
+	return frappe.utils.pdf.get_pdf(html)
+
+
+def save_and_attach(content, to_doctype, to_name):
+		file_name = "{}.pdf".format(to_name)
+		save_file(file_name, content, to_doctype, to_name, is_private=1)
 
 
 def get_print_format(item):
@@ -30,5 +41,4 @@ def is_doctype_allowed(doc):
 	"""
 	doctypes = frappe.get_list('NextCloud Print List')
 	allowed_doctypes = [doc.name for doc in doctypes]
-
-	return doc not in allowed_doctypes
+	return doc.doctype in allowed_doctypes
